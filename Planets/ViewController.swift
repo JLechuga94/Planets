cd43e437ade1cebc7c8a9d3c77e0299682b37fb7
Julian Lopez <jlechuga94@macbook-pro-de-julian.local>
2018-06-19 11:22:20 +0900

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Julian Lechuga Lopez on 19/6/18.
+//  Copyright © 2018 Julian Lechuga Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
